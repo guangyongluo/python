ed174645efa364a80c3c8c10116f204b46303486
luowei <luowei@tgqs.net>
2025-02-14 18:47:25 +0800

    luowei(python): sync local code.

--- python_pydantic/pydantic_tutorial.py
+++ python_pydantic/pydantic_tutorial.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from pydantic import BaseModel
 from typing import List, Optional
+from pydantic import BaseModel
 
 class User(BaseModel):
     id: int # 必填字段
---
+++ tutorial/__init__.py
@@ -0,0 +1,1 @@
+from .chapter03 import app03
